03d1f5e639a0a63740215d38846158c8ed789d28
hoseini <hoseini.1988@gmail.com>
2023-11-15 14:24:21 -0800

    تغییر پنل ادمین اضافه کردن پنل رستوران دار

---
+++ OnlineFood.WebHost/Areas/Restaurant/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineFood.WebHost.Areas.Restaurant.Controllers
+{
+    [Area("Restaurant")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
