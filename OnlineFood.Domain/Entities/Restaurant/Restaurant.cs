5bcb40be72c1e882f2661a48b83fd67fd98d4474
AmirBanaDev <amirbana220dev@gmail.com>
2023-11-19 17:54:00 +0100

    restaurant edit

--- OnlineFood.Domain/Entities/Restaurant/Restaurant.cs
+++ OnlineFood.Domain/Entities/Restaurant/Restaurant.cs
@@ -1,38 +1,35 @@
 ﻿using OnlineFood.Common.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OnlineFood.Domain.Entities.Base;
 
-namespace OnlineFood.Domain.Entities.Restaurant
+namespace OnlineFood.Domain.Entities.Resturant
 {
-    public class Restaurant
+    public class Restaurant : BaseEntity
     {
-        public int WorkTimeId { get; set; }
-        public int UserId { get; set; }
         public string Name { get; set; }
         public string Banner { get; set; }
-        public int CategoryId {  get; set; }
-        public string Description {  get; set; }
-        public bool IsActive {  get; set; }
-        public string PhoneNumber {  get; set; }
-        public string ManagerFullName {  get; set; }
-        public string ManagerPhone { get; set; }
-        public int NumberOfBranches {  get; set; }
-        public InterducedWayEnum InterduceWay { get; set; }
-        public ProvinceEnum Province { get; set; }
-        public int CityId { get; set; }
-        public string JoinDate {  get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public string ManagerFullName { get; set; }
+        public string ManagerPhoneNumber { get; set; }
+        public int NumberOfBranches { get; set; }
+        public InterduceWayEnum InterducedWay { get; set; }
+        public string Provice {  get; set; }
+        public string City {  get; set; }
+        public string SignUpDate {  get; set; }
         public bool DoesAcceptedByAdmin {  get; set; }
-        public float Score {  get; set; }
-        public string CreditAccountNum {  get; set; }
-        public string DeliveryPrice {  get; set; }
-        public string MinimumOrderPrice {  get; set; }
+        public int WorkTimeId { get; set; }
+        public int SocialMediaId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public float Score { get; set; }
+        public string DeliveryPrice { get; set; }
+        public string CreditAccountNumber { get; set; }
+        public string MinimumOrderPrice { get; set; }
         public string AverangeDeliveryTime { get; set; }
         public int Discount { get; set; }
-        public bool DoesSendOutOfArea {  get; set; }
-        public PaymentWayEnum PaymentWay { get; set; }
-        public int AddressId {  get; set; }
+        public bool DoesHaveOutOfAreaSend { get; set; }
+        public PayModelEnum PayModels { get; set; }
+        public int AddressId { get; set; }
     }
 }
