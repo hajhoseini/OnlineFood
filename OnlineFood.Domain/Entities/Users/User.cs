8788776d4ae1dc812bbfa0381d8fda3e821a11ce
unknown <alirezang7575@gmial.com>
2023-11-24 00:38:00 +0330

    develpo login and sigup in our project

--- OnlineFood.Domain/Entities/Users/User.cs
+++ OnlineFood.Domain/Entities/Users/User.cs
@@ -1,11 +1,19 @@
-﻿using OnlineFood.Domain.Entities.Base;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using OnlineFood.Domain.Entities.Base;
 
 namespace OnlineFood.Domain.Entities.Users;
 
-public class User : BaseEntity
+public class User :IdentityUser<string>
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    [Required]
+    [DefaultValue(2)]
+    public int RoleId { get; set; }
+    [Required] 
+    [DefaultValue(false)]
+    public bool AdminApp { get; set; }
+    [Required] 
+    [DefaultValue(true)]
+    public bool Status { get; set; }
 }
