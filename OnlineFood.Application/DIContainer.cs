d1338272e95a964d5f0ded3f5cb1ccfff6315ff9
hoseini <hoseini.1988@gmail.com>
2023-12-15 01:01:04 -0800

    Delete Services

--- OnlineFood.Application/DIContainer.cs
+++ OnlineFood.Application/DIContainer.cs
@@ -1,42 +1,12 @@
 ﻿using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using OnlineFood.Application.Services.Comments.Commands;
-using OnlineFood.Application.Services.Comments.Queries;
-using OnlineFood.Application.Services.Customers.Commands;
-using OnlineFood.Application.Services.Customers.Queries;
-using OnlineFood.Application.Services.Favorites.Commands;
-using OnlineFood.Application.Services.Favorites.Queries;
-using OnlineFood.Application.Services.OrderItems.Commands;
-using OnlineFood.Application.Services.OrderItems.Queries;
-using OnlineFood.Application.Services.Orders.Commands;
-using OnlineFood.Application.Services.Orders.Queries;
-using OnlineFood.Application.Services.Users.Commands;
-using OnlineFood.Application.Services.Users.Queries;
 
 namespace OnlineFood.Application;
 
 public static class DIContainer
 {
     public static IServiceCollection ApplicationServiceCollections(this IServiceCollection services)
-    {
-        services.AddScoped<IUserCommandService, UserCommandService>();
-        services.AddScoped<IUserQueryService, UserQueryService>();
-
-        services.AddScoped<ICommentCommandService, CommentCommandService>();
-        services.AddScoped<ICommentQueryService, CommentQueryService>();
-
-        services.AddScoped<ICustomerCommandService, CustomerCommandService>();
-        services.AddScoped<ICustomerQueryService, CustomerQueryService>();
-
-        services.AddScoped<IFavoriteCommandService, FavoriteCommandService>();
-        services.AddScoped<IFavoriteQueryService, FavoriteQueryService>();
-
-        services.AddScoped<IOrderItemCommandService, OrderItemCommandService>();
-        services.AddScoped<IOrderItemQueryService, OrderItemQueryService>();
-
-        services.AddScoped<IOrderCommandService, OrderCommandService>();
-        services.AddScoped<IOrderQueryService, OrderQueryService>();
-
+    {        
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
         return services;
