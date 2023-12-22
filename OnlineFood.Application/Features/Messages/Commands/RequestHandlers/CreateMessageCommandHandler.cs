d42faa0965c98346da5690a0d27fcac574ebd7b0
sa <694204+abolfathi@users.noreply.github.com>
2023-12-22 09:54:50 +0330

    ADD: CreateMessageCommandHandler
    Added CommandHandler for CreateMessage in Application's layer feature of messages

---
+++ OnlineFood.Application/Features/Messages/Commands/RequestHandlers/CreateMessageCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineFood.Domain.IRepositories;
+using OnlineFood.Application.Features.Messages.Commands.Requests;
+using OnlineFood.Domain.Entities.Messages;
+
+namespace OnlineFood.Application.Features.Messages.Commands.RequestHandlers
+{
+    public class CreateMessageCommandHandler:IRequestHandler<CreateMessageCommand,bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CreateMessageCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this._unitOfWork = unitOfWork;
+            this._mapper = mapper;
+        }
+
+        public async Task<bool> Handle(CreateMessageCommand request, CancellationToken cancellationToken)
+        {
+            var entity = _mapper.Map<Message>(request);
+            var result = await _unitOfWork.GenericReposity<Message>().Update(entity);
+            return result;
+        }
+    }
+}
