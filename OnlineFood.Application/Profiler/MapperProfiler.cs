7537f807727287cad4a01c1d101d99bc1ba1f68f
nafisbagheri <77045986+nafisbagheri@users.noreply.github.com>
2023-12-25 23:45:01 +0330

    14021004

--- OnlineFood.Application/Profiler/MapperProfiler.cs
+++ OnlineFood.Application/Profiler/MapperProfiler.cs
@@ -1,17 +1,25 @@
 ﻿using AutoMapper;
 using OnlineFood.Application.Dtos.Users;
+using OnlineFood.Application.Features.Cities.Commands.Requests;
 using OnlineFood.Application.Features.Comments.Commands.Requests;
 using OnlineFood.Application.Features.Customers.Commands.Requests;
 using OnlineFood.Application.Features.Favorites.Commands.Requests;
+using OnlineFood.Application.Features.MenuCategories.Commands.Requests;
 using OnlineFood.Application.Features.OrderItems.Commands.Requests;
 using OnlineFood.Application.Features.Orders.Commands.Requests;
+using OnlineFood.Application.Features.Provinces.Commands.Requests;
+using OnlineFood.Application.Features.RestaurantCategories.Commands.Requests;
 using OnlineFood.Application.Features.Trancations.Commands.Requests;
 using OnlineFood.Application.Features.Users.Commands.Requests;
+using OnlineFood.Domain.Entities.Cities;
 using OnlineFood.Domain.Entities.Comments;
 using OnlineFood.Domain.Entities.Customers;
 using OnlineFood.Domain.Entities.Favorites;
+using OnlineFood.Domain.Entities.MenuCategories;
 using OnlineFood.Domain.Entities.OrderItems;
 using OnlineFood.Domain.Entities.Orders;
+using OnlineFood.Domain.Entities.Provinces;
+using OnlineFood.Domain.Entities.RestaurantCategories;
 using OnlineFood.Domain.Entities.Trancations;
 using OnlineFood.Domain.Entities.Users;
 
@@ -43,5 +51,17 @@ public class MapperProfiler : Profile
 
         CreateMap<Order, CreateOrderCommand>().ReverseMap();
         CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+
+        CreateMap<MenuCategory, CreateMenuCategoryCommand>().ReverseMap();
+        CreateMap<MenuCategory, UpdateMenuCategoryCommand>().ReverseMap();
+
+        CreateMap<RestaurantCategory, CreateRestaurantCategoryCommand>().ReverseMap();
+        CreateMap<RestaurantCategory, UpdateRestaurantCategoryCommand>().ReverseMap();
+
+        CreateMap<City, CreateCityCommand>().ReverseMap();
+        CreateMap<City, UpdateCityCommand>().ReverseMap();
+
+        CreateMap<Province, CreateProvinceCommand>().ReverseMap();
+        CreateMap<Province, UpdateProvinceCommand>().ReverseMap();
     }
 }
