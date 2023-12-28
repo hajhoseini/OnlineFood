48a9cb339b23a02918350d5bcc3dcca9abc75b98
AmirBanaDev <amirbana220dev@gmail.com>
2023-12-28 21:40:27 +0100

    Create UI for restaurant register

--- OnlineFood.Common/Enums/InterduceWayEnum.cs
+++ OnlineFood.Common/Enums/InterduceWayEnum.cs
@@ -1,9 +1,16 @@
-﻿namespace OnlineFood.Common.Enums;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineFood.Common.Enums;
 
 public enum IntroduceWayEnum
 {
+    [Display(Name ="جست و جو در اینترنت")]
     SearchViaInternet,
+    [Display(Name = "معرفی توسط دوستان")]
     ByFriend,
-    SeeInAppStores,
-    Advertisement
+    [Display(Name ="تیلیغات")]
+    Advertisement,
+    [Display(Name ="سایر...")]
+    Others
 }
