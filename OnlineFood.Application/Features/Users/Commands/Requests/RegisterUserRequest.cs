368b93482a91d6d5d154a649b5a36d64b46a2353
unknown <alirezang7575@gmial.com>
2023-12-28 14:50:23 +0330

    loging page and user commnad and query was compeleted!

---
+++ OnlineFood.Application/Features/Users/Commands/Requests/RegisterUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using OnlineFood.Application.Dtos.Users;
+
+namespace OnlineFood.Application.Features.Users.Commands.Requests;
+
+public class RegisterUserRequest:IRequest<bool>
+{
+    public RegisterViewModel  Register { get; set; }
+}
---
+++ OnlineFood.Application/Features/Users/Queries/Requests/LoginQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using OnlineFood.Application.Dtos.Users;
+
+namespace OnlineFood.Application.Features.Users.Queries.Requests;
+
+public class LoginQuery:IRequest<bool>
+{
+    public LoginDto login { get; set; }
+}
