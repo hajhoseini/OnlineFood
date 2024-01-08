356b3d0530a4707b307b34593a8e349119be7333
sa <694204+abolfathi@users.noreply.github.com>
2024-01-09 00:11:46 +0330

    Update: RestaurantCategoryDTO

--- OnlineFood.Application/Dtos/RestaurantCategory/RestaurantCategoryDTO.cs
+++ OnlineFood.Application/Dtos/RestaurantCategory/RestaurantCategoryDTO.cs
@@ -1,11 +1,15 @@
-﻿using OnlineFood.Common.Enums;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 
-namespace OnlineFood.Application.Dtos.Message;
+namespace OnlineFood.Application.Dtos.RestaurantCategory;
 
 public class RestaurantCategoryDTO
 {
+    public int Id { get; set; }
+
+    [DisplayName("تاریخ")]
+    public DateTime CreateDate { get; set; }
+
     [Required]
     [DisplayName("عنوان")]
     public string Title { get; set; }
