11c3446a5e378a559b6627fd916b351553125f5b
hoseini <hoseini.1988@gmail.com>
2024-01-08 23:41:39 -0800

    Add Form For Register Customer

---
+++ OnlineFood.WebHost/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnlineFood.Application.Features.Customers.Commands.Requests;
+
+namespace OnlineFood.WebHost.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public RegisterController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] CreateCustomerCommand command)
+        {
+            var result = await mediator.Send(command);
+            return Ok(new { Data = result });
+        }
+    }
+}
