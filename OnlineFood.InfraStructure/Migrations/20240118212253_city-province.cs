5a60ce5ea7f24a4706ee7b5627ed819f57fd61f9
sa <694204+abolfathi@users.noreply.github.com>
2024-01-19 00:53:29 +0330

    migration city-province

---
+++ OnlineFood.InfraStructure/Migrations/20240118212253_city-province.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineFood.InfraStructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class cityprovince : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProvinceId",
+                table: "City",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_ProvinceId",
+                table: "City",
+                column: "ProvinceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_City_Province_ProvinceId",
+                table: "City",
+                column: "ProvinceId",
+                principalTable: "Province",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_City_Province_ProvinceId",
+                table: "City");
+
+            migrationBuilder.DropIndex(
+                name: "IX_City_ProvinceId",
+                table: "City");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ProvinceId",
+                table: "City",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
